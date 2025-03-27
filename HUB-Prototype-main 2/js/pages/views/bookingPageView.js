06d7e8da7a9e27ce53398b40f1b9c2ee76cf0b8b
DavidTB00 <davidtb17@hotmail.no>
2025-03-27 09:52:21 +0100

    Delete HUB-Prototype-main 2 directory

--- HUB-Prototype-main 2/js/pages/views/bookingPageView.js
+++
@@ -1,7 +0,0 @@
-export function bookingPageView() {
-    return /*HTML*/`
-    <h1>Booking</h1>
-    <iframe src="https://www.supersaas.com/schedule/EllieMarie/PrototypeTest" width="1500px" height="1000px" style="border: none;"></iframe>
-    
-    `;
-}
